c414ed931d56f4ecf0dd585e1e1571635b745eb1
Shuang Song <6882971+ssghost@users.noreply.github.com>
2020-06-14 18:24:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import getopt, sys
-from seqnet import *
+from seqnet import Seqnet
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 't:o:l:', ['text=','outdir=','loadmodel='])
+        opts, args = getopt.getopt(sys.argv[1:], 't:o:l:', ['text=','outpath=','modelpath='])
     except getopt.GetoptError as err:
         print(err) 
         sys.exit()
@@ -13,17 +13,18 @@ def main():
     for o, a in opts:
         if o in ('-t', '--text') and type(a)==str:
             tpath = a
-        elif o in ('-o', '--outdir') and type(a)==str:
+        elif o in ('-o', '--outpath') and type(a)==str:
             opath = a 
-        elif o in ('-l', '--loadmodel') and type(a)==str:
+        elif o in ('-l', '--modelpath') and type(a)==str:
             loadpath = a
         else:
             assert False, 'unhandled option'    
     
+    seq = Seqnet()
     if loadpath != None:
-        seqnet().load_model(loadpath)
+        seq.load_model(loadpath)
         
-    seqnet().test(tpath=tpath,opath=opath)
+    seq.test(tpath=tpath,opath=opath)
     
 if __name__ == "__main__":
     main()
